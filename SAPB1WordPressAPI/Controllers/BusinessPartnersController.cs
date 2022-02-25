f7655d48f11202750cf0cb9027273cfbf4ad0cf6
Jonathan Cuesto <34cuesto@gmail.com>
2022-02-24 23:49:48 -0400

    method customer statement added

--- SAPB1WordPressAPI/Controllers/BusinessPartnersController.cs
+++ SAPB1WordPressAPI/Controllers/BusinessPartnersController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using PuppeteerSharp;
+using PuppeteerSharp.Media;
 using SAPB1WordPressAPI.DataModel.DAL;
 using SAPB1WordPressAPI.DataModel.Entities;
 using System.Collections.Generic;
@@ -31,6 +33,25 @@ namespace SAPB1WordPressAPI.Web.Controllers
         public ActionResult<OCRD> GetBusinessPartnersCreditLimitByCardCode(string cardCode)
         {
             return sapDbContext.OCRD.AsNoTracking().Where(x => x.CardCode == cardCode).FirstOrDefault();
+        }
+
+        [HttpGet("[action]/cardCode")]
+        public async Task<IActionResult> GenerateCustomerStatement(string cardCode)
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+            await using var page = await browser.NewPageAsync();
+            await page.EmulateMediaTypeAsync(MediaType.Screen);
+            await page.SetContentAsync($"<div><h1>Hello PDF world!</h1><h2 style='color: red; text-align: center;'>Greeting <i>HTML</i> world</h2></div>");
+            var pdfContent = await page.PdfStreamAsync(new PdfOptions
+            {
+                Format = PaperFormat.A4,
+                PrintBackground = true
+            });
+            return File(pdfContent, "application/pdf", "converted.pdf");
         }
     }
 }
