5d5d8e86f5a10c6542267c6d1722ee131c637ab7
Jonathan Cuesto <34cuesto@gmail.com>
2022-02-25 01:33:20 -0400

    customer statement generated

--- SAPB1WordPressAPI/Controllers/BusinessPartnersController.cs
+++ SAPB1WordPressAPI/Controllers/BusinessPartnersController.cs
@@ -39,7 +39,9 @@ namespace SAPB1WordPressAPI.Web.Controllers
         [HttpGet("[action]")]
         public async Task<IActionResult> GenerateCustomerStatement(/*string cardCode , DateTime startDate, DateTime endDate*/)
         {
-            //var details = await this.sapDbContext.GetCustomerStatementAsync(cardCode, startDate, endDate);
+            var cardCode = "129564";
+            var startDate = new DateTime(2018, 01, 01);
+            var endDate = new DateTime(2022, 12, 31);
 
             await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
             await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
@@ -48,14 +50,36 @@ namespace SAPB1WordPressAPI.Web.Controllers
             });
             await using var page = await browser.NewPageAsync();
             await page.EmulateMediaTypeAsync(MediaType.Screen);
-            await page.SetContentAsync($"<div><h1>Hello PDF world!</h1><h2 style='color: red; text-align: center;'>Greeting <i>HTML</i> world</h2></div>");
+            await page.SetContentAsync(await GetHTMLBodyAsync(cardCode, startDate, endDate));
             var pdfContent = await page.PdfStreamAsync(new PdfOptions
             {
                 Format = PaperFormat.A4,
                 PrintBackground = true
             });
 
-            return File(pdfContent, "application/pdf", "converted.pdf");
+            return File(pdfContent, "application/pdf", $"{cardCode} - customerstatement.pdf");
+        }
+
+        private async Task<string> GetHTMLBodyAsync(string cardCode, DateTime startDate, DateTime endDate)
+        {
+            var details = await sapDbContext.GetCustomerStatementAsync(cardCode, startDate, endDate);
+            string rows = "";
+            string cardName="";
+
+            if(details != null)
+            {
+                cardName = details.First().CustomerName;
+            }
+
+            foreach (var dt in details)
+            {
+
+                rows += $"<tr>      <td> {dt.InvoicePostingDate.ToShortDateString()} </td>      <td> {dt.InvDocumentNumber} </td>      <td> {dt.DocTotal} </td>      <td> {dt.PaidToInvoice} </td>      <td> {dt.PaymentDocumentNumber}</td>      <td> {dt.PaymentPostingDate.ToShortDateString()} </td>      <td> {dt.RemainingPayment} </td>    </tr>";
+            }
+
+            string html = "<div>  <h1>Customer Statement</h1>  <h3>"+cardCode+" - "+cardName+"</h3>  <style>    th {      border-bottom: 2px solid black;    }  </style>  <table style='width: 100%'>    <!-- Inicio de encabezado -->    <tr>      <th>Invoice Posting Date</th>      <th>Inv Document Number</th>      <th>Doc Total</th>      <th>Paid To Invoice</th>      <th>Payment Document Number</th>      <th>Payment Posting Date</th>      <th>Remaining Payment</th>    </tr>    <!-- Fin de encabezado -->    <!-- Inicio de Columnas para datos -->    " + rows + "  </table></div>";
+
+            return html;
         }
     }
 }
